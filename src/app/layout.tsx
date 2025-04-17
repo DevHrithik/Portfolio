550febbb109122971ab52672b48a57348b1bb6ab
Hrithik Roy <hrithikroy1854@gmail.com>
2025-04-18 03:31:22 +0530

    Removed old Open Graph image and updated URLs in layout.tsx for consistency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://devhrithik.tech/og-image.png",
+        url: "https://devhrithik.tech/ogimage.png",
         width: 1200,
         height: 630,
         alt: DATA.name,
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
   twitter: {
     title: `${DATA.name}`,
     card: "summary_large_image",
-    images: ["https://devhrithik.tech/og-image.png"],
+    images: ["https://devhrithik.tech/ogimage.png"],
   },
   verification: {
     google: "",
